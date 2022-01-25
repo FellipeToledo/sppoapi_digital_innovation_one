00c6467fae9539cca1e0b5f602c508e285d53e83
Fellipe <fellipetoledo.dev@gmail.com>
2022-01-25 17:01:53 -0300

    sppo-api project added

---
+++ src/main/java/one/digitalinnovation/sppoapi/SppoapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.sppoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SppoapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SppoapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/one/digitalinnovation/sppoapi/SppoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.sppoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SppoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
